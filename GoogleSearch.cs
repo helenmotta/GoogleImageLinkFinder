2554ffc0e9aa6d8ab67fda6eac3ba6a457d5060d
Helen <helenmottab@gmai.com>
2023-06-23 21:03:06 -0300

    v1

---
+++ GoogleSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+public class GoogleSearch
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Digite sua pesquisa: ");
+        string? search = Console.ReadLine();
+
+        if (String.IsNullOrEmpty(search))
+        {
+            return;
+        }
+
+        string encodedQuery = Uri.EscapeDataString(search);
+        string searchUrl = $"https://www.google.com/search?q={encodedQuery}&tbm=isch&site=imghp";
+
+        GoogleSearch googleSearch = new GoogleSearch();
+        string searchResult = await googleSearch.ParseHtml(searchUrl);
+
+        Console.WriteLine(searchResult);
+
+    }
+
+    public async Task<string> ParseHtml(string link)
+    {
+        HtmlWeb web = new HtmlWeb();
+        HtmlDocument doc = new HtmlDocument();
+
+        try
+        {
+            doc = await web.LoadFromWebAsync(link);
+
+            string hrefContent = doc.DocumentNode.SelectNodes("//a").Where(a => a.InnerHtml.Contains("<img")).Select(b => b.Attributes["href"].Value).FirstOrDefault();
+            Regex regex = new Regex(@"imgurl=([^&]+)");
+            string imageUrl = regex.Match(hrefContent).Groups[1].Value;
+
+            return imageUrl;
+        }
+        catch
+        {
+            string errorMsg = "Erro ao consultar imagem";
+            return errorMsg;
+        }
+    }
+}
